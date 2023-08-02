52c58f532f0a8632a598063a0f03ae052241020c
Dragos Neacsu <dragosncs@gmail.com>
2023-08-02 18:36:55 +0300

    Initial Commit

---
+++ AstroMoon/AstroMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroMoonApp.swift
+//  AstroMoon
+//
+//  Created by Dragos Neacsu on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AstroMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
